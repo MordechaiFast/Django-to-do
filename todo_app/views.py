75d9c62e0f6c46129ab5b0df2de89fbc9b41c982
Mordechai Fast <mordechai.fast@gmail.com>
2022-04-04 22:46:51 +0300

    first view

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.views.generic import ListView
+from todo_app.models import ToDoItem, ToDoList
 
-# Create your views here.
+class ListListView(ListView):
+    """List View for the to-do lists themselves"""
+    model = ToDoList
+    template_name = 'todo_app/index.html'
+
+class ItemListView(ListView):
+    model = ToDoItem
+    template_name = 'todo_app/todo_list.html'
+
+    def get_queryset(self) -> 'QuerySet[T]':
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs['list_id'])
+    
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['todo_list'] = ToDoList.objects.get(id=self.kwargs['list_id'])
+        return context
